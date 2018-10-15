7eaae870778a9e0f3d89753399d2d9acda8992c0
Liu min <781775946@qq.com>
2018-10-15 17:58:13 +0800

    初步完成版

--- Js/index.js
+++ Js/index.js
@@ -2,21 +2,90 @@
  * Created by IisDean on 2018/10/14.
  */
 
-var site = new PCAS("ProvinceId","CityId");//通过ID省市二级联动，无默认值，无文字提示信息
+var site = new PCAS("ProvinceId", "CityId");//通过ID省市二级联动，无默认值，无文字提示信息
 
 var $newList = $(".J-new-list");
-function autoScroll(){
+function autoScroll() {
     var h = $newList.height();
-    if( h < $newList.parent().height() )return false;
+    if (h < $newList.parent().height())return false;
     $newList.animate({
-        'margin-top' : "-30px"
-    },1000,function(){
-        $newList.css({'margin-top' : "0px"}).find("li:first").appendTo($newList);
+        'margin-top': "-30px"
+    }, 1000, function () {
+        $newList.css({'margin-top': "0px"}).find("li:first").appendTo($newList);
         autoScroll();
     });
 }
 
 autoScroll();
 
-bodys.scrollTop = bodys.scrollHeight;
+var mySwiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    slidesPerView: 5,
+    initialSlide: 3,
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 20,
+        modifier: 5,
+        slideShadows: false
+    },
+})
 
+//滚动到顶部
+$(".J-goTop-btn").on("click", function () {
+    goToLocation('html,body', 0);
+});
+
+//滚动到菜单位置
+$(".J-goto-list li").on("click", function () {
+    var name = $(this).attr('data-name');
+    var h = $('div[data-location= '+ name +']').offset().top;
+    goToLocation('html,body', h);
+});
+
+//滚动到指定位置
+function goToLocation(obj, value) {
+    $(obj).animate({
+        scrollTop: value + 'px'
+    }, 300);
+}
+
+//一键定制
+$(".J-apply-btn").on("click",function(){
+    var name = $("#userName").val(),
+        mobile = $("#userMobile").val(),
+        province = $("#ProvinceId").val(),
+        cityId = $("#CityId").val();
+    if( name == '' )return alert('请输入姓名');
+    if( mobile.length < 11 )return alert('请输入手机号码');
+    if( province == '' )return alert('请选择省份');
+    if( cityId == '' )return alert('请选择市区');
+
+});
+
+
+//一键定制
+$(".J-apply-btn").on("click",function(){
+    var name = $("#userName").val(),
+        mobile = $("#userMobile").val(),
+        province = $("#ProvinceId").val(),
+        cityId = $("#CityId").val();
+    if( name == '' )return alert('请输入姓名');
+    if( mobile.length < 11 )return alert('请输入手机号码');
+    if( province == '' )return alert('请选择省份');
+    if( cityId == '' )return alert('请选择市区');
+
+});
+
+//马上预约
+$(".J-yuyue-btn").on("click",function(){
+    var name = $("#userName").val(),
+        mobile = $("#userMobile").val();
+    if( name == '' )return alert('请输入姓名');
+    if( mobile.length < 11 )return alert('请输入手机号码');
+
+
+});
--- Js/m_index.js
+++ Js/m_index.js
@@ -42,4 +42,32 @@ window.onload = function(){
     var site = new PCAS("ProvinceId","CityId");//通过ID省市二级联动，无默认值，无文字提示信息
     var site2 = new PCAS("ProvinceId2","CityId2");//通过ID省市二级联动，无默认值，无文字提示信息
 
+
+    //一键定制
+    $(".J-apply-btn").on("click",function(){
+        var name = $("#userName").val(),
+            mobile = $("#userMobile").val(),
+            province = $("#ProvinceId").val(),
+            cityId = $("#CityId").val();
+        if( name == '' )return alert('请输入姓名');
+        if( mobile.length < 11 )return alert('请输入手机号码');
+        if( province == '' )return alert('请选择省份');
+        if( cityId == '' )return alert('请选择市区');
+
+    });
+
+
+    //底部一键定制
+    $(".J-apply-btn2").on("click",function(){
+        var name = $("#userName2").val(),
+            mobile = $("#userMobile2").val(),
+            province = $("#ProvinceId2").val(),
+            cityId = $("#CityId2").val();
+        if( name == '' )return alert('请输入姓名');
+        if( mobile.length < 11 )return alert('请输入手机号码');
+        if( province == '' )return alert('请选择省份');
+        if( cityId == '' )return alert('请选择市区');
+
+    });
+
 }
